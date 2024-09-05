0a159ef3c24a9fe3e3c162c112a595bc4a15c435
2MACHETE <blackglitter@inbox.ru>
2024-09-05 19:19:25 +0300

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+void main()
+{
+	puts("         /\\");
+	puts("        /  \\     ");
+	puts("       /    \\        ");
+	puts("      /      \\      ");
+	puts("     /        \\     ");
+	puts("    /          \\     ");
+	puts("⣿⣿⣿⣿⣿⣿⣿⠿⠿⢛⣋⣙⣋⣩⣭⣭⣭⣭⣍⣉⡛⠻⢿⣿⣿⣿⣿");
+	puts("  ||            ||    ");
+	puts("  ||            ||     ");
+	puts("  ||   _______  ||     ");
+	puts("  ||   |     |  ||     123");
+	puts("  ||   |     |  ||     ");
+	puts("  ||   |_____|  ||     ");
+	puts("  ||            ||     ");
+	puts("  ||            ||     ");
+	puts("  || spaceship  ||      ");
+	puts("  ||    187     ||      ");
+	puts("  ||            ||     ");
+	puts("  ||            ||     ");
+	puts("  ||____________||      ");
+	puts(" /        v  v    \\     ");
+	puts("/  v  v         v  \\     ");
+	puts("     v    v               ");
+	puts(" v      v      v          ");
+	puts("    v   v   v             ");
+	puts("   v           v          ");
+}
