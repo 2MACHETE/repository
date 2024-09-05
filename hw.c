c7bfd1fca0788f63dd2cb9f92f8f1cc4eef195d9
2MACHETE <blackglitter@inbox.ru>
2024-09-05 18:42:48 +0300

    Add files via upload

---
+++ hw.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+void main()
+{
+	puts("9b.    .");
+	puts(" 4    qXb         .       dX                     Xb       .        dXp     t");
+	puts("dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb");
+	puts("9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP");
+	puts(" 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP");
+	puts("  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'");
+	puts("    `9XXXXXXXXXXXP' `9XX' Assassin  `98v8P'  Assassin   `XXP' `9XXXXXXXP'");
+	puts("        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~");
+	puts("                        )b.  .dbo.dP'`v'`9b.odb.  .dX(");
+	puts("                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.");
+	puts("                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb");
+	puts("                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb");
+	puts("                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP");
+	puts("                     `'      9XXXXXX(   )XXXXXXP      `'");
+	puts("                              XXXX X.`v'.X XXXX");
+	puts("                              XP^X'`b   d'`X^XX");
+	puts("                              X. 9  `   '  P )X      ");
+	puts("                              `b  `       '  d'  ");
+}
